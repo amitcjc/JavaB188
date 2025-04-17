bc26304b898cb58f61513a16c6eb2290b9fe7107
amitcjc <amitcjcpune@gmail.com>
2025-04-17 09:38:53 +0530

    by CJC

--- GitDemo/src/com/A.java
+++ GitDemo/src/com/A.java
@@ -2,7 +2,15 @@ package com;
 
 public class A 
 {
-public static void main(String[] args) {
+public static void main(String[] args) 
+{
 	System.out.println("Hello");
 }
+
+public void m1()
+{
+	System.out.println("This is M1");
+}
+
+
 }
